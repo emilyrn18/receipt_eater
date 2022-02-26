1a50423e8c0ccf011307fa69a4bf53180acf930d
emilyrn18 <emilyrn18@vt.edu>
2022-02-26 00:56:05 -0500

    Updated app.js to generate components

--- the_app/src/App.js
+++ the_app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Home,
+  User_Profile,
+  Receipt_Capture,
+  Finance_Bot,
+} from "./components";
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/User_Profile" element={<User_Profile />} />
+          <Route path="/Receipt_Capture" element={<Receipt_Capture />} />
+          <Route path="/Finance_Bot" element={<Finance_Bot />}/>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ the_app/src/components/index.js
@@ -0,0 +1,7 @@
+
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+export { default as User_Profile } from "./User_Profile";
+export { default as Receipt_Capture } from "./Receipt_Capture";
+export { default as Finance_Bot } from "./Finance_Bot";
