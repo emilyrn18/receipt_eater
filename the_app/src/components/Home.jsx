6cc58bd4e7b254c5f7ac914ae0cfa3eb9016ee9e
emilyrn18 <emilyrn18@vt.edu>
2022-02-26 13:48:48 -0500

    initial comit

--- the_app/src/components/Home.jsx
+++ the_app/src/components/Home.jsx
@@ -5,20 +5,22 @@ function Home() {
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Home</h1>
+            <br></br>
+            <h4>Inspiration:</h4>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+                Our inspiration for this app is, 
+                a lot of people struggle with keeping track of receipts and managing their finances. 
+                Better finance will lead to better financial decisions.
+            </p>
+            <h4>What it does:</h4>
+            <p>
+                This app will take in an image of a receipt and record necessary information 
+                such as the company the user bought something from, the item(s) they purchased, 
+                and the break down of the cost. It will then organize this spending based on type 
+                of purchase and the date it was purchased. We also hope to add a feature for 
+                users to ask questions about credit scores, loans, and tax information.
             </p>
           </div>
         </div>
--- the_app/src/components/User_Profile.jsx
+++ the_app/src/components/User_Profile.jsx
@@ -5,20 +5,12 @@ function User_Profile() {
     <div className="User_Profile">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">User_Profile</h1>
+            <h1 class="font-weight-light">User Profile</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              This page is meant to display a list of cards that show information 
+              for each receipt the user uploads onto the web app. This list can 
+              be filtered by date and subject.
             </p>
           </div>
         </div>
