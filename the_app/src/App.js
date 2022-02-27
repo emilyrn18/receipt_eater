d994de892b43978719b4f04782e0d71328443627
abrarr18 <abrarr18@vt.edu>
2022-02-26 23:43:59 -0500

    Working on login

--- the_app/src/App.js
+++ the_app/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
+
 // import firebase from "./firebase/compat/app";
 // import "firebase/compat/auth"
 // import "firebase/compat/firestore"
-
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
@@ -13,18 +13,43 @@ import {
   User_Profile,
   Receipt_Capture,
   Finance_Bot,
+  ReactCardSlider
 } from "./components";
 
 // const ref = firebase.firestore().collection("clients");
 
 function App(){
+  const adminuser = {
+    email: "abrarrhine@gmail.com",
+    password: "1234567"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""}); 
+  const [error, setError] = useState(""); 
+
+  const login= details =>{
+    console.log(details); 
+  }
+
+  const logout = ()=>{
+    console.log("Logout"); 
+  }
+
   return(
     <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome"> 
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+      ): (
+        <LoginForm></LoginForm>
+      )}
       <Router>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/User_Profile" element={<User_Profile />} />
+          <Route path="/User_Profile" element={<User_Profile />}/>
           <Route path="/Receipt_Capture" element={<Receipt_Capture />} />
           <Route path="/Finance_Bot" element={<Finance_Bot />}/>
         </Routes>
--- the_app/src/components/User_Profile.jsx
+++ the_app/src/components/User_Profile.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { ReactDOM } from "react";
 
 function User_Profile() {
   return (
     <div className="User_Profile">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">User Profile</h1>
-            <p>
-              This page is meant to display a list of cards that show information 
-              for each receipt the user uploads onto the web app. This list can 
-              be filtered by date and subject.
-            </p>
-          </div>
-        </div>
-      </div>
+      <h1 class="font-weight-light">User Profile</h1>
+      <Card 
+      img ="https://www.bing.com/images/search?view=detailV2&ccid=AUrlV8%2bc&id=37F1C92B8351933931879361A43FDF9B72558F1E&thid=OIP.AUrlV8-cM3Lxe1-71Pa-0AHaMT&mediaurl=https%3a%2f%2fexpressexpense.com%2fimages%2fgas-fuel-receipt-sample.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.014ae557cf9c3372f17b5fbbd4f6bed0%3frik%3dHo9VcpvfP6Rhkw%26pid%3dImgRaw%26r%3d0&exph=997&expw=600&q=image+of+gas+station+receipt&simid=608001716743856413&FORM=IRPRST&ck=77E51CADECE8EC0CF4944362982D4B6A&selectedIndex=15&ajaxhist=0&ajaxserp=0"/>
     </div>
   );
 }
 
 export default User_Profile;
+
+function Card(props){
+  return(
+    <div className = "card">
+      <div className="card__body">
+        <img src={props.img}></img>
+        <h2 className = "card__title"></h2>
+      </div>
+    </div>
+  )
+}
